cad5204aed079b368675553ce03e7934a53d335b
Pierce O'Sullivan <9221336+pierce-o@users.noreply.github.com>
2019-10-30 13:18:02 +0000

    Implemented UI to the reg addition page.

--- ottoman/src/app/entry-addition/registrations/registration.component.ts
+++ ottoman/src/app/entry-addition/registrations/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RegData } from 'src/models/regData';
 
 @Component({
   selector: 'app-registration',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class RegistrationComponent implements OnInit {
 
   regError: boolean = false;
+  tempRegdata: RegData = new RegData();
 
   constructor() { }
 
